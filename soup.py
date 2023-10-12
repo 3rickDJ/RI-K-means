9865fea5e88d55faf74eb2827f2761c202989ed2
3rick <erick.dejesus1@alumno.buap.mx>
2023-10-11 21:55:30 -0600

    feat: replace no words or numbers by an space

--- soup.py
+++ soup.py
@@ -8,7 +8,7 @@ soup = BeautifulSoup(html_doc, 'html.parser')
 print("-"*80)
 # title, desc = soup.head.title.get_text(), soup.head.meta.get('charset')
 pagina_txt = soup.body.get_text()
-# pagina_txt = re.sub(r'[^a-zA-Z0-9]+', ' ', pagina_txt)
+pagina_txt = re.sub(r'[^a-zA-Z0-9]+', ' ', pagina_txt)
 # pagina_txt = '\n'.join([ll.rstrip() for ll in pagina_txt.splitlines() if ll.strip()])
 with open('pagina2.txt', 'w') as f:
     f.write(pagina_txt)
