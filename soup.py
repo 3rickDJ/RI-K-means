90968030b4ee977532cdf29a7e33b0351ff8d492
3rick <erick.dejesus1@alumno.buap.mx>
2023-10-11 21:28:00 -0600

    feat: clean no words or numbers and store in pagina2.txt

--- soup.py
+++ soup.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 # html_doc = None
 with open('pagina.html', 'r') as f:
     html_doc = f.read()
@@ -6,6 +7,7 @@ soup = BeautifulSoup(html_doc, 'html.parser')
 print(soup.prettify())
 print("-"*80)
 pagina_txt = soup.get_text()
-pagina_txt = '\n'.join([ll.rstrip() for ll in pagina_txt.splitlines() if ll.strip()])
-with open('pagina.txt', 'w') as f:
+pagina_txt = re.sub(r'[^a-zA-Z0-9]+', ' ', pagina_txt)
+# pagina_txt = '\n'.join([ll.rstrip() for ll in pagina_txt.splitlines() if ll.strip()])
+with open('pagina2.txt', 'w') as f:
     f.write(pagina_txt)
