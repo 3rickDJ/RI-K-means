9be73efa6047e70644fd18d6650257b94fbe5248
3rick <erick.dejesus1@alumno.buap.mx>
2023-10-11 21:45:46 -0600

    feat: extract text from body
    TODO: extract title and description from head

--- soup.py
+++ soup.py
@@ -4,10 +4,11 @@ import re
 with open('pagina.html', 'r') as f:
     html_doc = f.read()
 soup = BeautifulSoup(html_doc, 'html.parser')
-print(soup.prettify())
+# print(soup.prettify())
 print("-"*80)
-pagina_txt = soup.get_text()
-pagina_txt = re.sub(r'[^a-zA-Z0-9]+', ' ', pagina_txt)
+# title, desc = soup.head.title.get_text(), soup.head.meta.get('charset')
+pagina_txt = soup.body.get_text()
+# pagina_txt = re.sub(r'[^a-zA-Z0-9]+', ' ', pagina_txt)
 # pagina_txt = '\n'.join([ll.rstrip() for ll in pagina_txt.splitlines() if ll.strip()])
 with open('pagina2.txt', 'w') as f:
     f.write(pagina_txt)
