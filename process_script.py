940eb5367d84414bae570cdf2b883567df8d6269
3rick <erick.dejesus1@alumno.buap.mx>
2023-10-15 18:09:03 -0600

    feat: matrix tf_idf

--- process_script.py
+++ process_script.py
@@ -1,5 +1,7 @@
 import process_text
 import json
+import pandas as pd
+import math
 
 def load_data():
     with open('quotes.json') as f:
@@ -27,10 +29,28 @@ def save_corpus_terms(corpus, flat_tokens):
 
 def save_corpus_stems(corpus, flat_tokens):
     corpus_stems = [d.stems for d in corpus]
-    stems = set([ stem for stems in corpus_stems for stem in stems ])
+    stems = list(set([ stem for stems in corpus_stems for stem in stems ]))
     with open('corpus_stems.txt', 'w') as f:
             f.write(' '.join(stems))
     return stems
+
+def save_matrix(corpus, stems_corpus):
+    table = []
+    for d in corpus:
+        row = dict()
+        for s in d.stems:
+            if s in stems_corpus:
+                row[s] = True
+        table.append(row)
+    return table
+
+def save_matrix_tf_idf(matrix, stems_corpus):
+    df = pd.DataFrame(matrix, index=range(len(matrix)), columns=stems_corpus)
+    df = df.fillna(False)
+    N = len(matrix)
+    idf = df.sum(axis=0).apply(lambda x: math.log(N / (1 + x)))
+    tf_idf = df.apply(lambda x: x * idf, axis=1)
+    return tf_idf
 
 
 
@@ -39,6 +59,12 @@ def main():
     tokens = save_corpus_tokens(corpus)
     terms = save_corpus_terms(corpus, tokens)
     stems = save_corpus_stems(corpus, tokens)
+    matrix = save_matrix(corpus, stems)
+    tf_idf = save_matrix_tf_idf(matrix, stems)
+    print(tf_idf)
+
+
+
     print("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥")
 
 
