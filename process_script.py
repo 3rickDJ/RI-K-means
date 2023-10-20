419b323fd380e264fd5042abedd1c0d0c3a02879
3rick <erick.dejesus1@alumno.buap.mx>
2023-10-19 18:36:39 -0600

    use output from script main items.json

--- process_script.py
+++ process_script.py
@@ -9,7 +9,7 @@ from collections import Counter #Libreria para contar elementos de una lista
 
 #Funcion para cargar los datos generados por quote_spider.py
 def load_data():
-    with open('quotes.json') as f:
+    with open('items.json') as f:
         data = json.load(f)
 
     corpus = []
@@ -49,7 +49,7 @@ def calculate_tf(corpus, stems_corpus):
         tf_matrix.append(row)
     return tf_matrix
 
-#Funcion para realizar la matriz tf-idf y guardar la matriz 
+#Funcion para realizar la matriz tf-idf y guardar la matriz
 def save_matrix_tf_idf(tf_matrix, stems_corpus):
     tf_df = pd.DataFrame(tf_matrix, columns=stems_corpus)
     N = len(tf_matrix)  # Número de documentos
@@ -69,36 +69,27 @@ def convert_query_to_vector(query, stems_corpus, idf):
 
     row = pd.DataFrame([row], columns=stems_corpus).fillna(0).apply(lambda x: x * idf, axis = 1)
     return row
-#Prueba para ejecutar el programa de crawler en la funcion main
-def Process_Script():
-    try:
-        # Llama al comando utilizando subprocess
-        subprocess.run(["scrapy", "crawl", "quotes", "-O", "quotes.json"], check=True)
-        print("Scraper ejecutado exitosamente.")
-    except subprocess.CalledProcessError as e:
-        print("Error al ejecutar el scraper:", e)
 
 #Funcion principal
 def main():
     #Llamamos al proceso
-    Process_Script()
     corpus = load_data() #Cargamos los datos
     tokens = save_corpus_tokens(corpus) #Guardamos los tokens
     terms = save_corpus_terms(corpus, tokens) #Guardamos los stopwords
     stems = save_corpus_stems(corpus, tokens) #Guardamos los stems
-    
+
     #Esta matriz sirve para contar las veces que aparece cada termino en cada documento
-    matrix = calculate_tf(corpus, stems) 
+    matrix = calculate_tf(corpus, stems)
     #Matriz tf-idf
-    tf_idf, idf = save_matrix_tf_idf(matrix, stems) 
-    
+    tf_idf, idf = save_matrix_tf_idf(matrix, stems)
+
     #Guardamos la matriz tf-idf en un archivo csv
     tf_idf.to_csv('tf_idf.csv', index=False)
     print(tf_idf)
-    
+
     k = 3   #Numero de clusters
     query = "I have not failed. I've just found 10,000 ways that won't work.".lower().strip()
-    query = re.split(r"[^a-z0-9]+", query) 
+    query = re.split(r"[^a-z0-9]+", query)
     # import dbg
     #import pudb; pudb.set_trace()
     query = process_text.ProcessData('query', query) #Procesamos la consulta aplicando stopwords y stems
@@ -111,4 +102,4 @@ def main():
     print("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥")
 
 if __name__ == "__main__":
-    main() 
+    main()
