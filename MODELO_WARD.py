a2c05afdea84b0f01a74cc59b4417ca32030e137
DiwsgenLop <diwsgen.lopez@alumno.buap.mx>
2023-11-19 19:45:03 -0600

    Clase para el modelo ward con librerias, estructurada

--- MODELO_WARD.py
+++ MODELO_WARD.py
@@ -4,11 +4,11 @@ import process_text
 import re
 #Librerias para crear el dendograma y metodo ward
 import matplotlib.pyplot as plt
-import scipy.cluster.hierarchy as shc
-from scipy.cluster.hierarchy import dendrogram, linkage, ward
+import scipy.cluster.hierarchy as sch
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 
 
-#Funcion para hacer de nuestra consulta un vector con tamañao igual al de la matriz booleana
+#Hacer de la consulta un vector
 def vector_consulta(consulta, corpus_stems):
     tokens = re.split(r"[^a-z0-9]+", consulta)
     query_data = process_text.ProcessData('query', tokens)
@@ -17,36 +17,53 @@ def vector_consulta(consulta, corpus_stems):
         if stem in query_data.stems:
             vector_query.append(1)
         else:
-            vector_query.append(0)
+            vector_query.append(0)        
     return np.array(vector_query)
 
-#Funcion para calcular el metodo ward
-def metodo_ward(boolean_matrix: pd.DataFrame):
-   cluestring=linkage(boolean_matrix,method='ward')
-   dendrogram=shc.dendrogram(cluestring)
-   plt.title('Dendograma')
-    
-#Mostrar los documentos donde esta la consulta anidada
-def get_result(query_raw: str, boolean_matrix: pd.DataFrame) -> list:
-    #### hacer vector query
-    corpus_stems = boolean_matrix.columns[1:]
-    q = vector_consulta(query_raw, corpus_stems)
-    #Realizar el metodo ward
-    metodo_ward(boolean_matrix)
-    #Determinar los documentos donde esta la consulta anidada
-    values = np.array(boolean_matrix.iloc[:,1:])
+#Realizar metodo ward y dendograma
+def ward(analisis_datos):
+    clusterting_jerarquico = linkage(analisis_datos, 'ward')
+    dendrograma = sch.dendrogram(clusterting_jerarquico)
+    plt.axhline(y=27, color='r', linestyle='--')
+    plt.savefig('dendrograma.jpg')  # Guardar el dendrograma como JPG
+    plt.show()
+    #Obtenemos los clusters
+    clusters = fcluster(clusterting_jerarquico, t=27, criterion='distance')
+    return clusters
+
+#Funcion para obtener los documentos que pertenecen al cluster de la consulta    
+def documentos(boolean_matrix):
+    num_cluster = boolean_matrix.iloc[-1,-1] #Obtener el numero de cluster de la consulta
+    cluster_query = boolean_matrix[boolean_matrix['Clusters'] == num_cluster] #De chill
+    names = cluster_query.iloc[:-1,0].values
+    list(names)
+    return names
 
 
 def main():
     boolean_matrix = pd.read_csv('tf.csv')
-    import pudb; pudb.set_trace()
     consulta_raw = 'In 1879, Albert Einstein was born in Ulm, Germany. He completed his Ph.D. at the University of Zurich by 1909. His 1905 paper explaining the photoelectric effect, the basis of electronics, earned him the Nobel Prize in 1921. His first paper on Special Relativity Theory, also published in 1905, changed the world.'
+    corpus_stems = boolean_matrix.columns[1:]
+    #Llamamos a la funcion vector_consulta
+    q = vector_consulta(consulta_raw, corpus_stems)
     
-    '''
-    print(resultado)
-    assert isinstance(resultado, list)
-    assert len(resultado) > 0
-    '''
+    #Agreagamos el vector de la consulta a la matriz booleana
+    consulta_df = pd.DataFrame([q], columns=corpus_stems, index=[boolean_matrix.index.max() + 1])# Crear un DataFrame para la consulta
+    consulta_df['name']='query'
+    # Agregar la consulta al conjunto de documentos
+    boolean_matrix = pd.concat([boolean_matrix, consulta_df])
+    
+    #Hacemos el dendograma pasando los valores de la matriz booleana
+    analisis_datos = boolean_matrix.iloc[:, 1:].values
+    #Clusters con el metodo ward
+    clusters = ward(analisis_datos)
+    #Agregamos los clusters a la matriz booleana
+    boolean_matrix['Clusters'] = clusters 
+    #Ahora comparamos los clusters y guardamos los documentos que son iguales
+    lista_documentos = documentos(boolean_matrix)
+    #Imprimimos los documentos que son iguales
+    print(lista_documentos)
+
 
 if __name__ == '__main__':
     main()
